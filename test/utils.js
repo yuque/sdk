2cb55a5982caba574daa32df4e5beb86729058c7
TZ <atian25@qq.com>
2019-03-25 11:53:12 +0800

    chore: trigger action

--- test/utils.js
+++ test/utils.js
@@ -1,9 +1,13 @@
 'use strict';
 
-const SDK = require('..');
+const Yuque = require('..');
 
+/**
+ * @param {Object} [options] - init options
+ * @return {Yuque} instance
+ */
 exports.getInstance = options => {
-  return new SDK(Object.assign({
+  return new Yuque(Object.assign({
     token: process.env.TOKEN,
   }, options));
 };
