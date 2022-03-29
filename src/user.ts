7d57b6e84334a6da1d4acf9bf7a106de3817b991
ConstasJ <2020212726@qq.com>
2022-03-29 16:22:07 +0800

    feat(user): 完成user.ts编写
    完成user.ts文件编写，并修改index.ts中对User的引用

--- src/user.ts
+++ src/user.ts
@@ -1,3 +1,15 @@
-export default class User{
+import {GArg, QArg} from "./index";
+import Client from "./client";
 
+export default class User {
+    private client: Client;
+
+    public constructor({client}: GArg) {
+        this.client = client;
+    }
+
+    public async get({login}: QArg = {}) {
+        const api = login ? `users/${login}` : 'user';
+        return this.client.request(api, {method: 'GET'});
+    }
 }
